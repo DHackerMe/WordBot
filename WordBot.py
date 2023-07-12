e4fa187d0badd13586d6f915ecfb52d4f015177e
DHackerMe <97457052+DHackerMe@users.noreply.github.com>
2023-07-12 20:46:29 +0200

    Main Script

---
+++ WordBot.py
@@ -0,0 +1,24 @@
+def find_words(letters):
+    words_found = []
+    letter_count = {letter: letters.count(letter) for letter in set(letters)}
+    
+    with open('words.txt', 'r') as file:
+        for line in file:
+            word = line.strip().lower()
+            word_count = {letter: word.count(letter) for letter in set(word)}
+            
+            if all(word_count.get(letter, 0) <= letter_count.get(letter, 0) for letter in set(word)) and len(word) <= len(letters):
+                words_found.append(word)
+    
+    return words_found
+
+user_letters = input("Input the letters: ").lower()
+
+words_found = find_words(user_letters)
+
+if words_found:
+    print("Words found:")
+    for word in words_found:
+        print(word)
+else:
+    print("No words found.")
